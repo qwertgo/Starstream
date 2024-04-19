4a1a64155833e3b3436b4f0106de3f28262346b3
Bennet Kurmann <kurmannbennet@gmail.com>
2024-04-19 17:39:25 +0200

    Added Correct Path Generation
    Fixed wrong positioning
    Added direction restrictions

--- Assets/Path Generation/_Scripts/PathGenerator.cs
+++ Assets/Path Generation/_Scripts/PathGenerator.cs
@@ -1,30 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public enum TransitionDirection {Left, Straight, Right}
+public enum PathDirection {Left, Straight, Right}
 public class PathGenerator : MonoBehaviour
 {
+    [Header("For Test Purposes")]
     [SerializeField]int pathLeght = 10;
+
+
+    [Header("")]
+
     [SerializeField]List<PathSection> pathSectionPrefabs;
     [SerializeField]PathSection startSection;
-    public List<PathSection> availablePathSections = new();
+    public List<PathSection> availablePathSections = new(); //temp public
+    List<PathSection> leftSections;
+    List<PathSection> rightSections;
     PathSection currentSection;
-    int pathTilt = 0;
-    int currentAngle;
     Queue<PathSection> activePaths = new();
+    float hexLength = 0.8660254f;
+    public Vector3 previousOffset = Vector3.zero; //temp public
+    public PathDirection pathDirection;
     void Start()
     {
         availablePathSections.AddRange(pathSectionPrefabs);
+        leftSections = pathSectionPrefabs.Where(obj => obj.GetDirection() == TransitionDirection.Left).ToList();
+        rightSections = pathSectionPrefabs.Where(obj => obj.GetDirection() == TransitionDirection.Right).ToList();
+
         //ComposePath();
         StartCoroutine(GenerateTest());
     }
     void ComposePath()
     {
         for(int i = 0;i < pathLeght; i++)
-        {
             GenerateSection();
-        }
     }
     void GenerateSection()
     {
@@ -33,29 +45,39 @@ public class PathGenerator : MonoBehaviour
             currentSection = Instantiate(startSection);
             activePaths.Enqueue(currentSection);
             currentSection.transform.eulerAngles = new Vector3(0,90,0);
+            pathDirection = PathDirection.Straight;
+            AddToFolder(currentSection);
+            return;
         }
-        CalculateAvailablePaths();
+        
         PathSection nextSection = Instantiate(availablePathSections[Random.Range(0,availablePathSections.Count)]);    
+        AddToFolder(nextSection);
         
-        
+        int angle = 0;
         switch (currentSection.GetDirection())
         {
             case TransitionDirection.Left:
-                TransformSection(-60,currentSection,nextSection);
-                pathTilt --;
-                break;
-            case TransitionDirection.Straight:
-                TransformSection(0,currentSection,nextSection);
+                angle = -60;
+                if(pathDirection ==PathDirection.Right)
+                    pathDirection = PathDirection.Straight;
+                else
+                    pathDirection = PathDirection.Left;
                 break;
             case TransitionDirection.Right:
-                TransformSection(60,currentSection,nextSection);
-                pathTilt ++;
+                angle = 60;
+                if(pathDirection == PathDirection.Left)
+                    pathDirection = PathDirection.Straight;
+                else
+                    pathDirection = PathDirection.Right;
                 break;
         }
+        TransformSection(angle,currentSection,nextSection);
+        CalculateAvailablePaths();
+
         if(!activePaths.Contains(currentSection))
             activePaths.Enqueue(currentSection);
         
-        if(activePaths.Count > 3)
+        if(activePaths.Count > 10)
         {
             Destroy(activePaths.Peek().gameObject);
             activePaths.Dequeue();
@@ -63,37 +85,50 @@ public class PathGenerator : MonoBehaviour
     }
     void TransformSection(int angle, PathSection curSection, PathSection nextSection)
     {
+        Vector3 offset = Vector3.zero;
         Quaternion rotation = Quaternion.Euler(0, angle, 0);
         nextSection.transform.rotation = curSection.transform.rotation * rotation;
+        Debug.Log($"Angle: {angle}");
 
-        Vector3 offset = Quaternion.Euler(0, angle, 0) * Vector3.forward * 1.73205080757f;
-        Debug.Log($"Rotation: {nextSection.transform.rotation.eulerAngles}, Offset: {offset}");
-
+        switch (pathDirection)
+        {
+            case PathDirection.Left:
+                offset = new Vector3(-1.5f,0,hexLength);
+                break;
+            case PathDirection.Straight:
+                offset = new Vector3(0,0,hexLength*2);
+                break;
+            case PathDirection.Right:
+                offset = new Vector3(1.5f,0,hexLength);
+                break;
+        }
         nextSection.transform.position = curSection.transform.position + offset;
-
         currentSection = nextSection;
+        previousOffset = offset;
     }
 
     void CalculateAvailablePaths()
     {
-        Debug.Log($"pathTilt: {pathTilt}");
         Debug.Log("current Directions: "+ currentSection.GetDirection());
-        if (pathTilt < 0)
+        if (currentSection.GetDirection() == TransitionDirection.Left)
         {
             availablePathSections.RemoveAll(section => section.GetDirection() == TransitionDirection.Left);
+            if(!availablePathSections.Intersect(rightSections).Any())
+                availablePathSections.AddRange(rightSections);
+
             Debug.Log("kicked left");
         }
-        else if (pathTilt > 0)
+        else if (currentSection.GetDirection() == TransitionDirection.Right)
         {
             availablePathSections.RemoveAll(section => section.GetDirection() == TransitionDirection.Right);
+            if(!availablePathSections.Intersect(leftSections).Any())
+                availablePathSections.AddRange(leftSections);
+
             Debug.Log("kicked right");
         }
-        else
-        {
-            availablePathSections = new List<PathSection>(pathSectionPrefabs);
-            Debug.Log("no pathTilt change");
-        }
     }
+    void AddToFolder(PathSection ps) => ps.transform.SetParent(GameObject.Find("Path Sections Folder").transform);
+
     IEnumerator GenerateTest()
     {
         while(true)
