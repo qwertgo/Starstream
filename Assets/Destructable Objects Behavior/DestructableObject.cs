90366a004c9824f145dee37b7c0cf3c856cf0673
Bennet Kurmann <kurmannbennet@gmail.com>
2024-04-26 12:02:19 +0200

    Added Collision Type

--- Assets/Destructable Objects Behavior/DestructableObject.cs
+++ Assets/Destructable Objects Behavior/DestructableObject.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class DestructableObject : MonoBehaviour
 {
-    [SerializeField] ParticleSystem particleSystem;
+    public enum CollisionType {Static, Moveable}
+    [SerializeField]CollisionType collisionType = CollisionType.Moveable;
+    [SerializeField]ParticleSystem particleSystem;
     MeshCollider meshCollider;
+    Rigidbody rb;
     private void Start() 
     {
+        rb = GetComponent<Rigidbody>();
+
         if(particleSystem == null && TryGetComponent<ParticleSystem>(out particleSystem) == true)
             particleSystem = GetComponent<ParticleSystem>();
         meshCollider = GetComponent<MeshCollider>();
+
+        if(collisionType == CollisionType.Static)
+            rb.isKinematic = true;
     }
     public void OnCrash()
     {
