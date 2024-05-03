375800c6bf85cc11b869e47ea52163423a474ebb
Samuel Galonska <samuelGalonska@protonmail.com>
2024-05-03 11:44:42 +0200

    Post Processing Anpassung + Player Collider angepasst

--- Assets/Materials/Shader/Outline.hlsl
+++ Assets/Materials/Shader/Outline.hlsl
@@ -24,20 +24,19 @@ static float sobelYMatrix[9] = {
 
 //This function runs the sobel algorithm over the depth texture
 void DepthSobel_float(float2 UV, float Thickness, 
-    out float Out, out float3 Normal){
+    out float Out){
     float2 sobel = 0;
 
     //We can unroll this loop to make it more efficient
     //the compiler is also smart enough to remove the i = 4 iteration, which is always 0
     [unroll] for(int i = 0; i < 9; i++){
-        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelSamplePoints[i] * Thickness);
+        float depth = SHADERGRAPH_SAMPLE_SCENE_NORMAL(UV + sobelSamplePoints[i] * Thickness);
 
         sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
     }
 
     //Get the final sobel value
     Out = length(sobel);
-    Normal = SHADERGRAPH_SAMPLE_SCENE_COLOR(UV);
 }
 
 void ColorSobel_float(float2 UV, float Thickness, 
